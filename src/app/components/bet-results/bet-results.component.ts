2be93b0bd5d29030ff8df7b650a0f9312487931d
astidolph <a.stidolph@gmail.com>
2022-05-23 15:49:32 +0100

    Add winnings to bet panel

--- src/app/components/bet-results/bet-results.component.ts
+++ src/app/components/bet-results/bet-results.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Horse } from 'src/app/classes/horse';
 import { PlayerBets, PlayerHorseBet } from 'src/app/classes/player-bets';
 import { BettingService } from 'src/app/services/betting.service';
+import { HorseManagementService } from 'src/app/services/horse-management.service';
 
 @Component({
   selector: 'app-bet-results',
@@ -9,9 +11,11 @@ import { BettingService } from 'src/app/services/betting.service';
 })
 export class BetResultsComponent {
   public playerBets: PlayerBets[] = [];
+  public winningHorse: Horse | undefined = undefined;
 
-  constructor(private bettingService: BettingService) {
+  constructor(private bettingService: BettingService, private horseManagementService: HorseManagementService) {
     this.playerBets = this.bettingService.playerBets;
+    this.winningHorse = this.horseManagementService.results[0];
   }
 
   public sumPlayerBets(bets: PlayerHorseBet[]) {
@@ -22,6 +26,19 @@ export class BetResultsComponent {
 
   public getHorsesBetOn(playerBets: PlayerBets): PlayerHorseBet[] {
     return playerBets.bets.filter(x => x.bet != 0);
+  }
+
+  public calculateWinnings(playerBets: PlayerHorseBet[]) {
+    var totalBet: number = playerBets.reduce((acc, val) => acc + val.bet, 0);
+    console.log('total' + totalBet);
+    var betOnWinningHorse = playerBets.find(x => x.horse.name === this.winningHorse?.name);
+
+    if (betOnWinningHorse === undefined) {
+      return -totalBet;
+    } else {
+      return (betOnWinningHorse.bet * betOnWinningHorse.horse.decimalOdds) - totalBet;
+    }
+
   }
 
 }

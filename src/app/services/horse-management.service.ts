5630c4eb8b306be60e7240bd2687487e9c7319da
astidolph <a.stidolph@gmail.com>
2022-09-15 09:05:27 +0100

    Add new horse styles

--- src/app/services/horse-management.service.ts
+++ src/app/services/horse-management.service.ts
@@ -52,7 +52,7 @@ export class HorseManagementService {
           percentageOdds: 0,
           fractionalOdds: "1:1",
           decimalOdds: 1.0,
-          colour: this.randomiseColour(),
+          colour: this.getDistinctColour(i),
           speed: 0,
           timingFunction: this.timingFunction()
         }
@@ -138,13 +138,18 @@ export class HorseManagementService {
     return speeds.sort((a, b) => a - b);
   }
 
-  private randomiseColour() {
-    const letters = '0123456789ABCDEF';
-    let colour = "#";
-    for (var i = 0; i < 6; i++) {
-      colour += letters[Math.floor(Math.random() * 16)];
-    }
-    return colour;
+  private getDistinctColour(colourIndex: number) {
+    const colourValues: string [] = [
+      "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000", 
+      "#800000", "#008000", "#000080", "#808000", "#800080", "#008080", "#808080", 
+      "#C00000", "#00C000", "#0000C0", "#C0C000", "#C000C0", "#00C0C0", "#C0C0C0", 
+      "#400000", "#004000", "#000040", "#404000", "#400040", "#004040", "#404040", 
+      "#200000", "#002000", "#000020", "#202000", "#200020", "#002020", "#202020", 
+      "#600000", "#006000", "#000060", "#606000", "#600060", "#006060", "#606060", 
+      "#A00000", "#00A000", "#0000A0", "#A0A000", "#A000A0", "#00A0A0", "#A0A0A0", 
+      "#E00000", "#00E000", "#0000E0", "#E0E000", "#E000E0", "#00E0E0", "#E0E0E0"
+    ];
+    return colourValues[colourIndex];
   }
 
   private generateOddsTable() {
